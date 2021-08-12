e9d6d1290c5368848c49f0ab15f0f7c6719ea3a6
machport <54745286+machport@users.noreply.github.com>
2021-08-12 12:33:08 -0700

    Create dahood.lua

---
+++ dahood.lua
@@ -0,0 +1,23 @@
+coroutine.wrap(function()
+while wait() do
+    for i,v in pairs(game.Players:GetPlayers()) do
+        pcall(function()
+            v.Character.LowerTorso:FindFirstChild("BOOMBOXSOUND").SoundId=""
+        end)
+    end
+end 
+end)()
+coroutine.wrap(function()
+    while wait(.3) do
+        coroutine.wrap(function()
+            pcall(function()
+                if game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild("BOOMBOXSOUND") then
+                    game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild("BOOMBOXSOUND"):Destroy()
+                end
+                game:GetService("ReplicatedStorage").MainEvent:FireServer("Boombox","694705467")
+                repeat wait() until game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild("BOOMBOXSOUND")
+                game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild("BOOMBOXSOUND").SoundId=""
+            end)
+        end)()
+    end
+end)()
